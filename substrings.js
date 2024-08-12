c9c513a71a9d931fd1a9b6aba4b2e46ac396612f
Lavanya Rapelli <lavanyarapelli555@gmail.com>
2024-08-13 03:50:46 +0530

    Strings and substrings

---
+++ substrings.js
@@ -0,0 +1,15 @@
+// finding allsubstrings
+
+function findAllSubstrings(str) {
+    let substrings = [];
+    for (let i = 0; i < str.length; i++) {
+        for (let j = i + 1; j <= str.length; j++) {
+            substrings.push(str.slice(i, j));
+        }
+    }
+    return substrings;
+}
+
+// Example usage
+let string = "abc";
+console.log(findAllSubstrings(string));
